414cf6b083bd8377fb4720ef3402b78af7391eb3
Seongchae <112529225+Seongchae@users.noreply.github.com>
2022-08-31 11:14:21 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    pins.analogWritePin(AnalogPin.P1, 400)
+    basic.showIcon(IconNames.SmallDiamond)
+})
+input.onButtonPressed(Button.AB, function () {
+    pins.analogWritePin(AnalogPin.P1, 0)
+    basic.showIcon(IconNames.No)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.analogWritePin(AnalogPin.P1, 1023)
+    basic.showIcon(IconNames.Diamond)
+})
+basic.forever(function () {
+	
+})
